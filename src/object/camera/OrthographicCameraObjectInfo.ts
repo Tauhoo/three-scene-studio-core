fa413a37723ce3b0b55ce718d2773cac8c4482f2
Tauhoo <tauhoo_ice@hotmail.com>
2025-02-11 23:29:15 +0700

    update projection matrix when camera is updated

--- src/object/camera/OrthographicCameraObjectInfo.ts
+++ src/object/camera/OrthographicCameraObjectInfo.ts
@@ -1,9 +1,28 @@
 import * as THREE from 'three'
 import { CameraObjectInfo } from './CameraObjectInfo'
+import { ObjectPath } from '../ObjectInfo'
 
 export class OrthographicCameraObjectInfo extends CameraObjectInfo {
   declare data: THREE.OrthographicCamera
   constructor(data: THREE.OrthographicCamera) {
     super(data)
+  }
+
+  setValue(objectPath: ObjectPath, value: any) {
+    const result = super.setValue(objectPath, value)
+    if (result.status === 'ERROR') {
+      return result
+    }
+
+    if (
+      objectPath.length === 1 &&
+      (objectPath[0] === 'left' ||
+        objectPath[0] === 'right' ||
+        objectPath[0] === 'top' ||
+        objectPath[0] === 'bottom')
+    ) {
+      this.data.updateProjectionMatrix()
+    }
+    return result
   }
 }
--- src/object/camera/PerspectiveCameraObjectInfo.ts
+++ src/object/camera/PerspectiveCameraObjectInfo.ts
@@ -1,9 +1,22 @@
 import * as THREE from 'three'
 import { CameraObjectInfo } from './CameraObjectInfo'
+import { ObjectPath } from '../ObjectInfo'
 
 export class PerspectiveCameraObjectInfo extends CameraObjectInfo {
   declare data: THREE.PerspectiveCamera
   constructor(data: THREE.PerspectiveCamera) {
     super(data)
+  }
+
+  setValue(objectPath: ObjectPath, value: any) {
+    const result = super.setValue(objectPath, value)
+    if (result.status === 'ERROR') {
+      return result
+    }
+
+    if (objectPath.length === 1 && objectPath[0] === 'aspect') {
+      this.data.updateProjectionMatrix()
+    }
+    return result
   }
 }
